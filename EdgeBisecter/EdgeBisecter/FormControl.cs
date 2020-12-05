bbcd64687f1a9672d45e12579fa4257d6c337000
Inwerwm <saclainvervm@gmail.com>
2020-12-05 16:49:11 +0900

    フォームを追加

---
+++ EdgeBisecter/EdgeBisecter/FormControl.cs
@@ -0,0 +1,34 @@
+﻿using PEPExtensions;
+using PEPlugin;
+using PEPlugin.Pmx;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EdgeBisecter
+{
+    public partial class FormControl : Form
+    {
+        IPERunArgs Args { get; }
+        IPXPmx Pmx { get; set; }
+
+        public FormControl(IPERunArgs args)
+        {
+            Args = args;
+
+            InitializeComponent();
+            Reload();
+        }
+
+        internal void Reload()
+        {
+            Pmx = Args.Host.Connector.Pmx.GetCurrentState();
+        }
+    }
+}
